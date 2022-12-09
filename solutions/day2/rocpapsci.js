44f0d91f701ca3c72525a392044187bc402e693a
Nicole Yip <nicole.yip@lego.com>
2022-12-09 18:05:36 +0000

    Day 2: Add check win logic, get shape score logic and running total

--- solutions/day2/rocpapsci.js
+++ solutions/day2/rocpapsci.js
@@ -1,9 +1,53 @@
 // sources
 //  * https://nodejs.org/api/readline.html#readline_example_read_file_stream_line_by_line
-//  * https://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
 
 const fs = require('node:fs');
 const readline = require('node:readline');
+
+let runningTotal = 0;
+
+function getShapeScore(shape) {
+    switch (shape) {
+        case 'X':
+            return 1;
+        case 'Y':
+            return 2;
+        case 'Z':
+            return 3;
+    }
+}
+
+function checkWin(gameArray) {
+    switch (gameArray[0]) {
+        case 'A':
+            switch (gameArray[2]) {
+                case 'X':
+                    return 'draw';
+                case 'Y':
+                    return 'win';
+                case 'Z':
+                    return 'lose';
+            }
+        case 'B':
+            switch (gameArray[2]) {
+                case 'X':
+                    return 'lose';
+                case 'Y':
+                    return 'draw';
+                case 'Z':
+                    return 'win';
+            }
+        case 'C':
+            switch (gameArray[2]) {
+                case 'X':
+                    return 'win';
+                case 'Y':
+                    return 'lose';
+                case 'Z':
+                    return 'draw';
+            }
+    }
+}
 
 async function processLineByLine() {
   const fileStream = fs.createReadStream('input.txt');
@@ -16,8 +60,20 @@ async function processLineByLine() {
   // ('\r\n') in input.txt as a single line break.
 
   for await (const line of rl) {
-    console.log(line.split(" ").toString());
+    let gameInput = line.split(" ").toString();
+    let outcomeScore = 0;
+    switch (checkWin(gameInput)) {
+        case 'win':
+            outcomeScore = 6;
+            break;
+        case 'draw':
+            outcomeScore = 3;
+            break;
+    }
+    console.log(`outcomeScore: ${outcomeScore}`);
+    runningTotal += parseInt(outcomeScore) + parseInt(getShapeScore(gameInput[2]));
   }
+  console.log(runningTotal);
 }
 
 processLineByLine();
