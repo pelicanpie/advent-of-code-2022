424a293a385310e4f4c8bd86c28576be10764a17
Nicole Yip <nicole.yip@lego.com>
2022-12-19 18:49:34 +0000

    Day4: Read input file

---
+++ solutions/day4/findOverlaps.js
@@ -0,0 +1,25 @@
+// sources
+//  * https://nodejs.org/api/readline.html#readline_example_read_file_stream_line_by_line
+
+const fs = require('node:fs');
+const readline = require('node:readline');
+
+let runningTotal = 0;
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  for await (const line of rl) {
+      console.log(`${line}`);
+  }
+  console.log(runningTotal);
+}
+
+processLineByLine();
